aabd17896c5e478b334772123c82bae9cc56db82
oleg <oleg.1998@mail.ru>
2019-03-11 22:50:23 +0200

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oleg
+ * Date: 11.03.2019
+ * Time: 20:56
+ */
+
+namespace App\Http\Controllers;
+
+
+class LoginController extends Controller
+{
+
+}
